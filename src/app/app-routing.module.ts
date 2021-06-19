ff32e4750e0e345eed36c92ab11dc6c2a3b397b2
daivedl <daived@live.com.ar>
2021-06-19 12:46:13 -0300

    creando componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductosComponent } from './components/productos/productos.component';
+import { WhereComponent } from './components/where/where.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'productos', component: ProductosComponent },
+  { path: 'where' , component: WhereComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { DetalleProductoComponent } from './components/detalle-producto/detalle-producto.component';
+import { WhereComponent } from './components/where/where.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductosComponent,
+    DetalleProductoComponent,
+    WhereComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
